75213bb69269766b76eabd47f28728d24f490156
Nikhil Laad <nikhillaad12@gmail.com>
2018-08-08 12:17:16 +0530

    Added Enqueue() function

--- WeightItem/WeightItem/Program.cs
+++ WeightItem/WeightItem/Program.cs
@@ -46,7 +46,9 @@ namespace WeightItem
             else return 0;
         }
     }
-
+    //For any parent node in the list at index[pi], the two child nodes are located at [2*pi + 1]
+    //and [2*pi +2]
+    //For a given child node, at index[ci], its parent is located at [ci-1]/2
     public class PriorityQueue<T> where T : IComparable<T>
     {
         private List<T> data;
@@ -54,5 +56,21 @@ namespace WeightItem
         {
             this.data = new List<T>();
         }
+        public void Enqueue(T item)
+        {
+            data.Add(item);
+            int ci = data.Count - 1;
+            while (ci > 0)
+            {
+                int pi = (ci - 1) / 2;
+                if (data[ci].CompareTo(data[pi]) >= 0)
+                    break;
+                T tmp = data[ci];
+                data[ci] 
+                
+
+            }
+        }
     }
+    
 }
